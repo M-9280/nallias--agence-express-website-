6b0cf02c8dadf386b057ba0c1e5b7f2e3cb14b0d
M-9280 <nalliasagenceexpress@gmail.com>
2025-07-03 22:22:12 +0000

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -8,5 +8,16 @@ const rootElement = document.getElementById('root')
 createRoot(rootElement).render(
   <StrictMode>
     <App />
+  </StrictMode> import React, { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+const rootElement = document.getElementById('root')
+
+createRoot(rootElement).render(
+  <StrictMode>
+    <App />
   </StrictMode>
+);
 )
